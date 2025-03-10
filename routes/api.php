02bd8bd90318afeb46227dbe04f01e9e5d905e7f
Juan Gonzalez <juan.gonzalez09@uceva.edu.co>
2025-03-10 01:17:51 -0500

    front compilado

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\CurrencyExchangeController;
 use App\Http\Controllers\HistorialController;
 use Illuminate\Support\Facades\Route;
 
+
+
 Route::get('/weather/{id}', [WeatherController::class, 'consultarApiWeather']);
 Route::get('/currency', [CurrencyExchangeController::class, 'consultarApiCurrency']);
 Route::get('/ciudades', [CiudadesController::class, 'index']);
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\WeatherController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any}', function () {
+    return view('index');
+})->where('any', '.*');
